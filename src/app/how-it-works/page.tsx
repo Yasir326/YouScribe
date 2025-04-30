22e4d336dc31dee0f9b6fc75388abd52ad2177a8
yasir-dg <Yasir.Khan@domesticandgeneral.com>
2025-04-30 08:41:08 +0100

    return null if user not found

--- src/app/how-it-works/page.tsx
+++ src/app/how-it-works/page.tsx
@@ -4,5 +4,6 @@ import HowItWorksClient from './HowItWorksClient';
 export default async function HowItWorks() {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
+  if (!user) return null;
   return <HowItWorksClient user={user} />;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 const HomePage = async () => {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
+  if (!user) return null;
   return <HomePageClient user={user} suppressHydrationWarning />;
 };
 
